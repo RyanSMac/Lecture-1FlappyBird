4e6a4d27c94591e4a8f4fb6f4bf56a8cb90f92e8
RyanSMac <ryanmcconville93@gmail.com>
2020-03-22 16:16:49 +0000

    Infinate Pipe Update
    Added a Pipe class
    Spawn pipes after a certian amount of time has passed
    These pipes a spawn at random heights

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ Class = require 'class'
 
 require 'Bird'
 
+require 'Pipe'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -22,6 +24,13 @@ local GROUND_SCROLL_SPEED = 60
 local BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
+
+-- Track all the pipes
+local pipes = {}
+
+-- Timer for pipe spawning
+local spawnTimer = 0
+local spawnRate = 2
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -64,7 +73,23 @@ function love.update(dt)
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
         % VIRTUAL_WIDTH
 
+    spawnTimer = spawnTimer + dt
+
+    if spawnTimer > spawnRate then
+        table.insert(pipes, Pipe())
+        print('Added new pipe!')
+        spawnTimer = 0
+    end
+
     bird:update(dt)
+
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+
+        if pipe.x < -pipe.width then
+            table.remove(pipes, k)
+        end
+    end
 
     love.keyboard.keysPressed = {}
 end
@@ -73,6 +98,11 @@ function love.draw()
     push:start()
 
     love.graphics.draw(background, -backgroundScroll, 0)
+
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
+
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
     bird:render()
